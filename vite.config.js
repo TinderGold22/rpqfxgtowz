7df7242452888f0622511f2fdd1e081d38ebce05
TinderGold22 <TinderGold22@example.com>
2025-08-02 13:28:50 -0400

    Remove vite-plugin-md to fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mdPlugin from 'vite-plugin-md'
+
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),         // React plugin
-    mdPlugin(),      // Markdown (.md) support
+
   ],
   resolve: {
     alias: {
@@ -16,6 +16,5 @@ export default defineConfig({
     extensions: [".js", ".jsx", ".md"], // optional, to support .md import
   },
   optimizeDeps: {
-    include: ["vite-plugin-md"],
-  },
+
 })
