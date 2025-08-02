f43bc577c7184fb05c162965a3270f2db5a50d69
TinderGold22 <tindergold22@example.com>
2025-08-02 14:47:33 -0400

    Fix date-fns compatibility and remove vite-plugin-md

--- vite.config.js
+++ vite.config.js
@@ -13,12 +13,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-
+    extensions: [".js", ".jsx"],
   },
 
 })
-
-
-
-    extensions: [".js", ".jsx"],
-
